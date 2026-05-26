ad682905979924ca5c35a5ad4aaee2c310922cca
Prof <vincent.leduc@collegeahuntsic.qc.ca>
2026-05-26 14:45:50 -0400

    QuestionView Implementation

--- OpenQuizz/View/QuestionView.swift
+++ OpenQuizz/View/QuestionView.swift
@@ -9,27 +9,50 @@ import UIKit
 
 class QuestionView: UIView {
 
-    
-    // Only override draw() if you perform custom drawing.
-    // An empty implementation adversely affects performance during animation.
-    override func draw(_ rect: CGRect) {
-        // Drawing code
-    }
-    
-    @IBOutlet private var label: UILabel?
-    @IBOutlet private var icon: UIImageView?
+  
+    @IBOutlet private var label: UILabel!
+    @IBOutlet private var icon: UIImageView!
     	
     
-    override init(frame: CGRect) {
-        super.init(frame: frame)
-        print(label?.font)
-     
+    override func awakeFromNib() {
+            super.awakeFromNib()
+            print(label.font) // ✅ Ici, label et icon NE SONT PAS nil
+            print(icon.image)
+            title = "Test"
+            setStyle(.incorrect)
     }
-
-    required init?(coder: NSCoder) {
-        super.init(coder: coder)
-        print(label?.font)
-
+    
+    enum Style {
+        case correct, incorrect, standard
+    }
+    var title = ""
+       {
+           didSet {
+               label.text = title
+           }
+       }
+       
+       var style : Style = .standard {
+           didSet {
+               setStyle(style)
+           }
+       }
+       
+     private func setStyle(_ style: Style) {
+           switch style {
+           case .correct:
+               backgroundColor = UIColor(red: 200.0/255.0, green: 236.0/255.0, blue: 160.0/255.0, alpha: 1) // Vert
+               //backgroundColor = #colorLiteral(red: 0.4666666687, green: 0.7647058964, blue: 0.2666666806, alpha: 1)
+               icon.image = UIImage(named: "Icon Correct")
+               icon.isHidden = false
+        case .incorrect:
+            backgroundColor = UIColor(red: 243.0/255.0, green: 135.0/255.0, blue: 148.0/255.0, alpha: 1) // Rouge
+            icon.isHidden = false
+            icon.image = UIImage(named: "Icon Error")
+        case .standard:
+            backgroundColor = UIColor(red: 191.0/255.0, green: 196.0/255.0, blue: 201.0/255.0, alpha: 1) // Gris
+            icon.isHidden = true
+        }
     }
 
 }
